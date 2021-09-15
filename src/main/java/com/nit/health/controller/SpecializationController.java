c6b4224a3cccf65f1c69cc9a858893e0d8eaa227
Shubhesh <shubheshshukla7@gmail.com>
2021-09-15 14:59:11 +0530

    Delete updated in controller

--- src/main/java/com/nit/health/controller/SpecializationController.java
+++ src/main/java/com/nit/health/controller/SpecializationController.java
@@ -1,10 +1,19 @@
 package com.nit.health.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.nit.health.entity.Specialization;
 import com.nit.health.service.ISpecializationService;
 
 @Controller
@@ -17,6 +26,30 @@ public class SpecializationController {
 	@GetMapping("/register")
 	public String displayRegister() {
 		return "SpecializationRegister";
+	}
+	
+	@PostMapping("/save")
+	public String saveForm(@ModelAttribute Specialization specialization,Model model) {
+		Long id=service.saveSpecialization(specialization);
+		String message="Record ("+id+") Inserted";
+		model.addAttribute("message", message);
+		return "SpecializationRegister";	
+		}
+	
+	@GetMapping("/all")
+	public String ViewAll(Model model , @RequestParam(value = "message", required = false)String message) {
+		List<Specialization> list=service.getAllSpecializations();
+		model.addAttribute("list", list);
+		model.addAttribute("message", message);
+		return "SpecializationData";
+		
+	}
+	
+	@GetMapping("/delete")
+	public String deleteSpecialization(@RequestParam Long id, RedirectAttributes attributes) {
+		service.removeSpecialization(id);
+		attributes.addAttribute("message", "Record ("+id+") Deleted");
+		return "redirect:all";
 	}
 
 }
